442f77dd74fa8334a844a9a5820042a915702a55
Christopher.Kwok <ChristopherTylerKwok@gmail.com>
2025-02-16 11:21:52 -0500

    Add method to write username to the config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,30 +2,64 @@ package config
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"path"
 )
 
 type Config struct {
-	DBURL string `json:"db_url"`
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+// Get the full path to the config file
+func getConfigPath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("could not get path: %e", err)
+	}
+	fullPath := path.Join(homeDir, FileName)
+	return fullPath, nil
 }
 
 func Read() (Config, error) {
-	homeDir, err := os.UserHomeDir()
+	fullPath, err := getConfigPath()
 	if err != nil {
 		return Config{}, err
 	}
 
-	fullPath := path.Join(homeDir, FileName)
+	// Read from file
 	fileData, err := os.ReadFile(fullPath)
 	if err != nil {
-		return Config{}, err
+		return Config{}, fmt.Errorf("could not read config: %e", err)
 	}
 
 	var config Config
 	if err := json.Unmarshal(fileData, &config); err != nil {
-		return Config{}, err
+		return Config{}, fmt.Errorf("failed to unmarshal data: %e", err)
 	}
 
 	return config, nil
+}
+
+func SetUser(userName string) error {
+	confData, err := Read()
+	if err != nil {
+		return err
+	}
+	confData.CurrentUserName = userName
+	bytes, err := json.Marshal(confData)
+	if err != nil {
+		return err
+	}
+	configPath, err := getConfigPath()
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(configPath, bytes, 0666)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -13,4 +13,14 @@ func main() {
 	} else {
 		fmt.Println(confData)
 	}
+	err = config.SetUser("lane")
+	if err != nil {
+		fmt.Println(err)
+	}
+	confData, err = config.Read()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(confData)
+	}
 }
