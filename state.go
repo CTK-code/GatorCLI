589433845e1e5f73b7e19ae62fb0bb4cb2cde517
Christopher.Kwok <ChristopherTylerKwok@gmail.com>
2025-02-16 14:59:23 -0500

    Add database to program state

--- state.go
+++ state.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/CTK-code/GatorCLI/internal/config"
+import (
+	"github.com/CTK-code/GatorCLI/internal/config"
+	"github.com/CTK-code/GatorCLI/internal/database"
+)
 
 type state struct {
+	db     *database.Queries
 	Config *config.Config
 }
