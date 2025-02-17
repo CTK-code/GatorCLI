18453967736126dc743ada09a2bced26cbadc65f
Christopher.Kwok <ChristopherTylerKwok@gmail.com>
2025-02-17 11:16:37 -0500

    Fix bug where user not deleted on reset
    User was not being removed due to a no ON DELETE CASCADE on the foreign
    key constraint in the feeds table.

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -5,9 +5,9 @@ CREATE TABLE feeds (
   updated_at TIMESTAMP NOT NULL,
   name VARCHAR(50) NOT NULL,
   url VARCHAR(100) NOT NULL,
-  user_id UUID,
+  user_id UUID NOT NULL,
   CONSTRAINT user_fk FOREIGN KEY (user_id)
-    REFERENCES users(id),
+    REFERENCES users(id) ON DELETE CASCADE,
   UNIQUE(url)
 );
 
